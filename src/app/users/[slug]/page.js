42d2d41118ab2c219236359092889631df939db2
Maikon Weber <maikonweber@gmail.com>
2024-07-10 21:56:26 -0300

    Add Header and startup project

---
+++ src/app/users/[slug]/page.js
@@ -0,0 +1,9 @@
+export default function Users({ params }) {
+   
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+         { params.slug}
+      </main>
+    );
+  }
+  
---
+++ src/component/Header/index.js
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <header className="bg-red-800 w-100 h-12 text-white p-4">
+        
+            Header
+        </header> 
+    )
+}
+
+
+module.exports = {
+    Header
+}
